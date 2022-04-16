f3e4e9eda77eec2e4076a2853e6c11b20ac2a0f6
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 21:16:51 +0000

    xxx

--- cpp_classifier/cpp_classifier.h
+++ cpp_classifier/cpp_classifier.h
@@ -10,7 +10,7 @@ namespace cpp_classifier {
 struct Classifier {
   Classifier(const std::string& fn);
   float Classify(unsigned char const * data,int h,int w) const;
-
+  ~Classifier();
   private:
   std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter {nullptr, TfLiteInterpreterDelete};
   std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{nullptr, TfLiteInterpreterOptionsDelete};
