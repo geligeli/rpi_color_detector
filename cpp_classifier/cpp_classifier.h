bfcf68b65839ffb13f30ac4b56104dc955c41b6a
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 20:28:44 +0000

    xxx

--- cpp_classifier/cpp_classifier.h
+++ cpp_classifier/cpp_classifier.h
@@ -12,9 +12,9 @@ struct Classifier {
   float Classify(unsigned char const * data,int h,int w) const;
 
   private:
-  std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter{};
-  std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{};
-  std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model{};
+  std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter {nullptr, TfLiteInterpreterDelete};
+  std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{nullptr, TfLiteInterpreterOptionsDelete};
+  std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model{nullptr, TfLiteModelDelete};
   TfLiteTensor *inputTensor{};
   const TfLiteTensor *outputTensor{};
 };
