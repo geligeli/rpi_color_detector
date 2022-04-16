db547230dc6568f7936bfaf858c9c5558d253d7c
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 21:21:06 +0000

    xxx

--- cpp_classifier/cpp_classifier.cpp
+++ cpp_classifier/cpp_classifier.cpp
@@ -9,13 +9,21 @@
 namespace cpp_classifier {
 
 Classifier::Classifier(const std::string& fn) {
-  model = {TfLiteModelCreateFromFile(fn.c_str()), TfLiteModelDelete}; // "/nfs/general/shared/adder.tflite");
-  options = {TfLiteInterpreterOptionsCreate(), TfLiteInterpreterOptionsDelete};
-  TfLiteInterpreterOptionsSetNumThreads(options.get(), 4);
-  interpreter = {TfLiteInterpreterCreate(model.get(), options.get()), TfLiteInterpreterDelete};
+  // model = {TfLiteModelCreateFromFile(fn.c_str()), TfLiteModelDelete}; // "/nfs/general/shared/adder.tflite");
+  // options = {TfLiteInterpreterOptionsCreate(), TfLiteInterpreterOptionsDelete};
+  // TfLiteInterpreterOptionsSetNumThreads(options.get(), 4);
+  // interpreter = {TfLiteInterpreterCreate(model.get(), options.get()), TfLiteInterpreterDelete};
 
-  auto* inputTensor = TfLiteInterpreterGetInputTensor(interpreter.get(), 0);
-  const auto* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter.get(), 0);
+  // auto* inputTensor = TfLiteInterpreterGetInputTensor(interpreter.get(), 0);
+  // const auto* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter.get(), 0);
+
+  model = TfLiteModelCreateFromFile(fn.c_str());
+  options = TfLiteInterpreterOptionsCreate();
+  TfLiteInterpreterOptionsSetNumThreads(options, 4);
+  interpreter = TfLiteInterpreterCreate(model, options);
+  auto* inputTensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
+  const auto* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
+
 
   std::cerr << "Input tensor type=";
   std::cerr << TfLiteTypeGetName(inputTensor->type) << std::endl;
@@ -31,8 +39,8 @@ Classifier::Classifier(const std::string& fn) {
 }
 
 float Classifier::Classify(unsigned char const* data, int h, int w) const {
-  auto* inputTensor = TfLiteInterpreterGetInputTensor(interpreter.get(), 0);
-  const auto* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter.get(), 0);
+  auto* inputTensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
+  const auto* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
 
 
   std::cerr << reinterpret_cast<uintptr_t>(inputTensor) << std::endl;
@@ -45,14 +53,14 @@ float Classifier::Classify(unsigned char const* data, int h, int w) const {
   std::cerr << h << " " << w << " " << 3 << std::endl;
   TfLiteTensorCopyFromBuffer(inputTensor, data, h*w*3);
 
-  std::cerr << "call interpreter" << std::endl;
-  TfLiteInterpreterInvoke(interpreter.get());
-  float y[2];
-  std::cerr << "get result" << std::endl;
-  TfLiteTensorCopyToBuffer(outputTensor, y, sizeof(y));
+  // std::cerr << "call interpreter" << std::endl;
+  // TfLiteInterpreterInvoke(interpreter.get());
+  // float y[2];
+  // std::cerr << "get result" << std::endl;
+  // TfLiteTensorCopyToBuffer(outputTensor, y, sizeof(y));
 
-  std::cerr << "done result" << std::endl;
-  return y[0];
+  // std::cerr << "done result" << std::endl;
+  // return y[0];
 
   // float r = intercept;
   // for (const auto& c : coefs) {
--- cpp_classifier/cpp_classifier.h
+++ cpp_classifier/cpp_classifier.h
@@ -12,9 +12,14 @@ struct Classifier {
   float Classify(unsigned char const * data,int h,int w) const;
   ~Classifier();
   private:
-  std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter {nullptr, TfLiteInterpreterDelete};
-  std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{nullptr, TfLiteInterpreterOptionsDelete};
-  std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model{nullptr, TfLiteModelDelete};
+  // std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter {nullptr, TfLiteInterpreterDelete};
+  // std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{nullptr, TfLiteInterpreterOptionsDelete};
+  // std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model{nullptr, TfLiteModelDelete};
+
+  TfLiteInterpreter* interpreter;
+  TfLiteInterpreterOptions* options;
+  TfLiteModel* model;
+
   // TfLiteTensor *inputTensor{};
   // const TfLiteTensor *outputTensor{};
 };
