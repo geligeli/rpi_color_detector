c3d1da6d3a7e0dbb24cc8e9e6a0a17a547cba887
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 20:43:11 +0000

    xxx

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 pushd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 make color_detect
 popd
-sudo ./build/color_detect ./http_server 1234
+sudo gdb --args ./build/color_detect ./http_server 1234
