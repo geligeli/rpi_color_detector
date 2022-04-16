1ddfaaf42d76bf2f7404f79d484fce19f4ce2327
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 20:28:01 +0000

    xxx

--- cpp_classifier/cpp_classifier.h
+++ cpp_classifier/cpp_classifier.h
@@ -8,32 +8,15 @@
 namespace cpp_classifier {
 
 struct Classifier {
-  std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter;
-  std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options;
-  std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model;
+  void LoadFromFile(const std::string& fn);
+  float Classify(unsigned char const * data,int h,int w) const;
 
+  private:
+  std::unique_ptr<TfLiteInterpreter,  void(*)(TfLiteInterpreter*)> interpreter{};
+  std::unique_ptr<TfLiteInterpreterOptions,  void(*)(TfLiteInterpreterOptions*)> options{};
+  std::unique_ptr<TfLiteModel, void(*)(TfLiteModel*)> model{};
   TfLiteTensor *inputTensor{};
   const TfLiteTensor *outputTensor{};
-
-
-  // TfLiteInterpreterDelete(interpreter);
-  // TfLiteInterpreterOptionsDelete(options);
-  // TfLiteModelDelete(model);
-
-  // TfLiteInterpreterOptions *options = TfLiteInterpreterOptionsCreate();
-  // TfLiteInterpreterOptionsSetNumThreads(options, numThreads);
-  // TfLiteInterpreter *interpreter = TfLiteInterpreterCreate(model, options);
-
-  // TfLiteInterpreterAllocateTensors(interpreter);
-
-  // TfLiteTensor *inputTensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
-
-  // std::cout << TfLiteTypeGetName(inputTensor->type) << std::endl;
-
-
-  void LoadFromFile(const std::string& fn);
-
-  float Classify(unsigned char const * data,int h,int w) const;
 };
 
 }  // namespace cpp_classifier
