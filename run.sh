daff9bf3f017db3ee3cf247c661f8e344a6179cb
Stephan Gammeter <sgammeter@gmail.com>
2022-04-16 20:56:01 +0000

    xxx

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ pushd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make color_detect
 popd
-sudo ./build/color_detect ./http_server 1234
+sudo LD_PRELOAD=/usr/lib/gcc/arm-linux-gnueabihf/10/libasan.so ./build/color_detect ./http_server 1234
